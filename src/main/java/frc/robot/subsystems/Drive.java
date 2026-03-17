6b20806cb62ce92e08bb76dfe12eb7acc9e84166
lonaslee <93890846+lonaslee@users.noreply.github.com>
2026-03-16 22:07:06 -0500

    yagsl works; todo: get abs encoder offsets

--- src/main/java/frc/robot/subsystems/Drive.java
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -7,11 +7,15 @@ import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import swervelib.SwerveDrive;
 import swervelib.parser.SwerveParser;
+import swervelib.telemetry.SwerveDriveTelemetry;
+import swervelib.telemetry.SwerveDriveTelemetry.TelemetryVerbosity;
+
 
 public class Drive extends SubsystemBase {
     public final SwerveDrive swerveDrive;
 
     public Drive() {
+        SwerveDriveTelemetry.verbosity = TelemetryVerbosity.HIGH;
         try {
             swerveDrive = new SwerveParser(Filesystem.getDeployDirectory()).createSwerveDrive(4);
         } catch (IOException e) {
