a9d2a7e0a4f5417a2aed3a968cc9f43f3d140b60
lonaslee <93890846+lonaslee@users.noreply.github.com>
2026-03-18 17:38:56 -0500

    shooter 2 motors work

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -4,8 +4,8 @@ import com.revrobotics.PersistMode;
 import com.revrobotics.ResetMode;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.config.SparkBaseConfig;
 import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -13,12 +13,19 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Shooter extends SubsystemBase {
     private final SparkMax motor;
+    private final SparkMax motor2;
 
     public Shooter() {
         motor = new SparkMax(20, MotorType.kBrushless);
         motor.configure(new SparkMaxConfig()
-                .idleMode(SparkBaseConfig.IdleMode.kCoast)
+                .idleMode(IdleMode.kCoast)
                 .inverted(false),
+                ResetMode.kResetSafeParameters,
+                PersistMode.kPersistParameters);
+        motor2 = new SparkMax(21, MotorType.kBrushless);
+        motor2.configure(new SparkMaxConfig()
+                .follow(motor, true)
+                .idleMode(IdleMode.kCoast),
                 ResetMode.kResetSafeParameters,
                 PersistMode.kPersistParameters);
     }
